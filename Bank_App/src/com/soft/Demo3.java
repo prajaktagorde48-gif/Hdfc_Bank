d156bf48bff7b5bc0e18731a7bbab3b0af238171
prajaktagorde48-gif <prajaktagorde48@gmail.com>
2026-01-11 18:53:33 +0530

    new qa file changes

--- Bank_App/src/com/soft/Demo3.java
+++ Bank_App/src/com/soft/Demo3.java
@@ -5,6 +5,6 @@ public class Demo3 {
 	public static void main(String[] args) {
 	    System.err.println("welcome jii");
 		System.err.println("welcome to QA branch");
-		int a = 100;
+		int a = 20;
 	}
 }
